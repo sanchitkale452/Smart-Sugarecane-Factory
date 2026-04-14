6c44b9b1115fc1a2dfa3cd36194f16b2af97bb8e
sanchitkale452 <sanchitkale452@gmail.com>
2026-04-14 20:53:34 +0530

    seo

--- sugarcane_factory/urls.py
+++ sugarcane_factory/urls.py
@@ -5,9 +5,12 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.conf.urls.i18n import i18n_patterns
 from django.views.i18n import set_language
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
 ]
 
 
